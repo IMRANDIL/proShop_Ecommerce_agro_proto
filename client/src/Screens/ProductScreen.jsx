1c5297989da695c153fa35e2b8c9fee8af0cfd3e
IMRANDIL <aliimranadil2@gmail.com>
2022-06-16 14:45:17 +0530

    yh

--- client/src/Screens/ProductScreen.jsx
+++ client/src/Screens/ProductScreen.jsx
@@ -1,12 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link, useParams } from "react-router-dom";
 import { Row, Col, Image, ListGroup, Card, Button } from "react-bootstrap";
 import Rating from "../components/Rating";
-import products from "../products";
+import axios from "axios";
 
 const ProductScreen = () => {
+  const [product, setProduct] = useState({});
+
   const { id } = useParams();
-  const product = products.find((p) => p._id === id);
+  // const product = products.find((p) => p._id === id);
+
+  const fetchSingleProduct = async () => {
+    const { data } = await axios.get(
+      `http://localhost:5000/api/products/${id}`
+    );
+    setProduct(data);
+  };
+
+  useEffect(() => {
+    fetchSingleProduct();
+  }, []);
 
   return (
     <>
