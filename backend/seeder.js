9b2ddf1bd1fa648c39636bc0d9257f6b0f2b2bda
IMRANDIL <aliimranadil2@gmail.com>
2022-06-16 19:44:22 +0530

    yh

---
+++ backend/seeder.js
@@ -0,0 +1,57 @@
+import dotenv from "dotenv";
+import dbConnection from "./config/db.js";
+import mongoose from "mongoose";
+import User from "./models/userModel.js";
+import Order from "./models/orderModel.js";
+import Product from "./models/productModel.js";
+import colors from "colors";
+import users from "./data/users.js";
+import products from "./data/data.js";
+dotenv.config();
+
+dbConnection();
+
+const importData = async () => {
+  try {
+    await Order.deleteMany();
+
+    await Product.deleteMany();
+    await User.deleteMany();
+
+    const createdUses = await User.insertMany(users);
+    const adminUser = createdUses[0]._id;
+    const sampleProducts = products.map((product) => {
+      return {
+        ...product,
+        user: adminUser,
+      };
+    });
+
+    await Product.insertMany(sampleProducts);
+    console.log("Data Imported".green.inverse);
+    process.exit();
+  } catch (error) {
+    console.error(`Error: ${error}`.red.inverse);
+    process.exit(1);
+  }
+};
+
+const destroyData = async () => {
+  try {
+    await Order.deleteMany();
+
+    await Product.deleteMany();
+    await User.deleteMany();
+    console.log("Data Destroyed".red.inverse);
+    process.exit();
+  } catch (error) {
+    console.error(`Error: ${error}`.red.inverse);
+    process.exit(1);
+  }
+};
+
+if (process.argv[2] === "-d") {
+  destroyData();
+} else {
+  importData();
+}
