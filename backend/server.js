bd02eb76715220ccbf96270df33df10a31d399b7
IMRANDIL <aliimranadil2@gmail.com>
2022-06-17 08:16:01 +0530

    yh

--- backend/server.js
+++ backend/server.js
@@ -1,50 +1,30 @@
-import dotenv from 'dotenv';
-import dbConnection from './config/db.js';
-import colors from 'colors'
+import dotenv from "dotenv";
+import dbConnection from "./config/db.js";
+import colors from "colors";
 dotenv.config();
 
 dbConnection();
 
+import express from "express";
 
-
-import express from 'express';
-import products from './data/data.js';
-
-import cors from 'cors';
-
+import cors from "cors";
+import productRouter from "./router/productRoute.js";
 const app = express();
-
-
 
 //some middleware...
 
-app.use(cors())
+app.use(cors());
 
+app.use("/api/products", productRouter);
 
-
-
-app.get('/',(req,res)=>{
-    res.send('API is ready')
-})
-
-
-app.get('/api/products',(req,res)=>{
-   res.json(products)
+app.get("/", (req, res) => {
+  res.send("API is ready");
 });
 
+const PORT = process.env.PORT || 7000;
 
-
-app.get('/api/products/:id',(req,res)=>{
-    const product = products.find((p)=>p._id === req.params.id);
-    res.json(product)
- });
-
-
-
-
-const PORT=process.env.PORT || 7000;
-
-
-app.listen(PORT,()=>{
-    console.log(`server runs in ${process.env.NODE_ENV} mode on port: ${PORT}😃`.yellow.bold);
-})
+app.listen(PORT, () => {
+  console.log(
+    `server runs in ${process.env.NODE_ENV} mode on port: ${PORT}😃`.yellow.bold
+  );
+});
