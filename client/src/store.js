b15fc2e5d16deeef30391a38211bfe843e06626b
IMRANDIL <aliimranadil2@gmail.com>
2022-06-17 09:42:00 +0530

    yh

--- client/src/store.js
+++ client/src/store.js
@@ -4,8 +4,11 @@ import { composeWithDevTools } from "redux-devtools-extension";
 
 const reducer = combineReducers({});
 const initialState = {};
+const middleware = [thunk];
 const store = createStore(
   reducer,
   initialState,
-  composeWithDevTools(applyMiddleware)
+  composeWithDevTools(applyMiddleware(...middleware))
 );
+
+export default store;
