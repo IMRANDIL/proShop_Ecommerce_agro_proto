8dcd9abfd2a6696c887e7f46f5160210f77f62dc
IMRANDIL <aliimranadil2@gmail.com>
2022-06-18 09:05:53 +0530

    yh

--- client/src/Screens/CartScreen.jsx
+++ client/src/Screens/CartScreen.jsx
@@ -1,6 +1,35 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  Link,
+  useParams,
+  useNavigate,
+  useSearchParams,
+} from "react-router-dom";
+import {
+  Row,
+  Col,
+  ListGroup,
+  Image,
+  Form,
+  Button,
+  Card,
+} from "react-bootstrap";
+import { addToCart } from "../actions/cartActions";
+import Message from "../components/Message";
 
 const CartScreen = () => {
+  const [searchParams] = useSearchParams();
+  const navigation = useNavigate();
+  const { id } = useParams();
+  const qty = searchParams.get("qty");
+  const dispatch = useDispatch();
+  const { cartItems } = useSelector((state) => state.cart);
+
+  useEffect(() => {
+    dispatch(addToCart(id, qty));
+  });
+
   return <div>CartScreen</div>;
 };
 
