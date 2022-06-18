5983a68b8cd7130d7817690e784aa6ad279afafb
IMRANDIL <aliimranadil2@gmail.com>
2022-06-18 09:49:04 +0530

    yh

--- client/src/Screens/CartScreen.jsx
+++ client/src/Screens/CartScreen.jsx
@@ -27,10 +27,58 @@ const CartScreen = () => {
   const { cartItems } = useSelector((state) => state.cart);
 
   useEffect(() => {
-    dispatch(addToCart(id, qty));
+    if (id) {
+      dispatch(addToCart(id, qty));
+    }
   }, [dispatch, id, qty]);
 
-  return <div>CartScreen</div>;
+  return (
+    <Row>
+      <Col md={8}>
+        <h1>Shopping Cart</h1>
+        {cartItems?.length === 0 ? (
+          <Message>
+            Your Cart is Empty <Link to="/">Go Back</Link>
+          </Message>
+        ) : (
+          <ListGroup variant="flush">
+            {cartItems.map((item) => (
+              <ListGroup.Item key={item.product}>
+                <Row>
+                  <Col md={2}>
+                    <Image src={item.image} alt={item.name} fluid rounded />
+                  </Col>
+                  <Col md={3}>
+                    <Link to={`/product/${item.product}`}>{item.name}</Link>
+                  </Col>
+                  <Col md={2}>${item.price}</Col>
+                  <Col md={2}>
+                    <Form.Control
+                      as="select"
+                      value={item.qty}
+                      onChange={(e) =>
+                        dispatch(
+                          addToCart(item.product, Number(e.target.value))
+                        )
+                      }
+                    >
+                      {[...Array(item.countInStock).keys()].map((x) => (
+                        <option key={x + 1} value={x + 1}>
+                          {x + 1}
+                        </option>
+                      ))}
+                    </Form.Control>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+            ))}
+          </ListGroup>
+        )}
+      </Col>
+      <Col md={2}></Col>
+      <Col md={2}></Col>
+    </Row>
+  );
 };
 
 export default CartScreen;
