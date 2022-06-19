51a866d2e682db0e97fc99682c97484306717ef0
IMRANDIL <aliimranadil2@gmail.com>
2022-06-19 07:46:55 +0530

    YH

--- client/src/actions/userActions.js
+++ client/src/actions/userActions.js
@@ -37,3 +37,40 @@ export const userLogin = (email, password) => async (dispatch) => {
     });
   }
 };
+
+export const userRegister = (name, email, password) => async (dispatch) => {
+  try {
+    dispatch({
+      type: USER_REGISTER_REQUEST,
+    });
+
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+
+    const { data } = await axios.post(
+      `http://localhost:5000/api/users`,
+      { name, email, password },
+      config
+    );
+    dispatch({
+      type: USER_REGISTER_SUCCESS,
+      payload: data,
+    });
+
+    dispatch({
+      type: USER_LOGIN_SUCCESS,
+    });
+    localStorage.setItem("userInfo", JSON.stringify(data));
+  } catch (error) {
+    dispatch({
+      type: USER_REGISTER_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
