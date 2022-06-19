6bf4c63230b002a7c4f4d4a207112242fbc7f481
IMRANDIL <aliimranadil2@gmail.com>
2022-06-19 07:55:59 +0530

    yh

--- client/src/Screens/LoginScreen.jsx
+++ client/src/Screens/LoginScreen.jsx
@@ -1,12 +1,18 @@
 import React, { useState, useEffect } from "react";
+import { Link, useNavigate, useSearchParams } from "react-router-dom";
 import { userLogin } from "../actions/userActions";
 import Message from "../components/Message";
 import Loader from "../components/Loader";
 import { useDispatch, useSelector } from "react-redux";
-import {} from "react-bootstrap";
+import { Form, Button, Row, Col } from "react-bootstrap";
+import FormContainer from "../components/FormContainer";
 
 const LoginScreen = () => {
-  return <div>LoginScreen</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const dispatch = useDispatch();
+  return <FormContainer></FormContainer>;
 };
 
 export default LoginScreen;
