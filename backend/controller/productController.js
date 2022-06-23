1d1c85af4865e41363430b9c71d8aff1d4642dc7
IMRANDIL <aliimranadil2@gmail.com>
2022-06-23 20:11:14 +0530

    yh

--- backend/controller/productController.js
+++ backend/controller/productController.js
@@ -29,3 +29,48 @@ export const deleteProductById = asyncHandler(async (req, res) => {
     throw new Error("Product not found");
   }
 });
+
+//private/admin..
+
+export const createProduct = asyncHandler(async (req, res) => {
+  const product = new Product({
+    name: "sample name",
+    price: 0,
+    user: req.user._id,
+    image: "/images/sample.jpg",
+    brand: "sample brand",
+    category: "sample category",
+    countInStock: 0,
+    numReviews: 0,
+    description: "sample description",
+  });
+
+  const createdProduct = await product.save();
+
+  res.status(201).json(createProduct);
+});
+
+//private/admin..
+
+export const updateProductById = asyncHandler(async (req, res) => {
+  const { name, price, description, image, brand, category, countInStock } =
+    req.body;
+
+  const product = await Product.findById(req.params.id);
+
+  if (product) {
+    product.name = name;
+    product.price = price;
+    product.description = description;
+    product.image = image;
+    product.brand = brand;
+    product.category = category;
+    product.countInStock = countInStock;
+
+    const updatedProduct = await product.save();
+    res.json(updatedProduct);
+  } else {
+    res.status(404);
+    throw new Error("Product not found");
+  }
+});
