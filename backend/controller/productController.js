3308522a585139eb46df410e4c1874030289041c
IMRANDIL <aliimranadil2@gmail.com>
2022-06-26 19:39:36 +0530

    yh

--- backend/controller/productController.js
+++ backend/controller/productController.js
@@ -2,7 +2,7 @@ import Product from "../models/productModel.js";
 import asyncHandler from "express-async-handler";
 
 export const getAllProducts = asyncHandler(async (req, res) => {
-  const pageSize = 2;
+  const pageSize = 10;
   const page = Number(req.query.pageNumber) || 1;
   const keyword = req.query.keyword
     ? {
